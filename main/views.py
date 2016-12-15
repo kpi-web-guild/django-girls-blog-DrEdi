055386c5e922f7aac48386d25bb621dc10830dc4
DrEdi <grigoriy.rubel@gmail.com>
2016-12-15 21:44:18 +0200

    Add css
    Add css file
    Fix files that were blocked in previous pull-request

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 
+
 def post_list(request):
-	posts = Post.objects.all()
-	return render(request, 'main/index.html', {'posts': posts})
+    posts = Post.objects.all()
+    return render(request, 'main/index.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'main/post_detail.html', {'post': post})
