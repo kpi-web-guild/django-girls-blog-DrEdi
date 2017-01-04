3e30f39630cbefcefef169e332f81ef6646f15cd
DrEdi <grigoriy.rubel@gmail.com>
2017-01-04 12:48:28 +0200

    Add vagga integration

--- blog/settings.py
+++ blog/settings.py
@@ -1,7 +1,9 @@
 """Django settings for blog project."""
 
 import os
-import dj_database_url
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -66,7 +68,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default':  dj_database_url.config()
+    'default':  env.db()
 }
 
 
