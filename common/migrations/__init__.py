0db4091b8060e8321e711525922936e0b4d6313e
DrEdi <grigoriy.rubel@gmail.com>
2017-01-17 14:25:07 +0200

    Describe migration package in common

--- common/migrations/__init__.py
+++ common/migrations/__init__.py
@@ -1,1 +1,7 @@
-"""This file is needed for migration admin to DB."""
+"""
+Package for creating admin account.
+
+When you run project with pgsqlDB, admin model
+automatically migrate to DB. After you can login with
+name and password you specify in settings.
+"""
