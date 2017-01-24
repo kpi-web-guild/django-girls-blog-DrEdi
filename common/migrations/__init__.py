fd568ee7bf090c8326c007429c799bbe6a49df83
Grigoriy <grigoriy.rubel@gmail.com>
2017-01-24 02:49:16 +0200

    Add integration with pgsql in vagga (#9)
    * Add integration with pgsql in vagga
    
    Add new app where we create admin for db
    Integrate vagga with pgsql
    Refresh reirements
    
    * Describe migration package in common
    
    * Divide commands in vagga
    
    * Add environ-file to container declaration
    
    * Specify minimum version of vagga
    
    Close #9

---
+++ common/migrations/__init__.py
@@ -0,0 +1,8 @@
+"""
+Fixture for admin account.
+
+This will automatically add pre-configured admin account
+to your db during migration. Specify admin's email via
+ADMIN_EMAIL environment variable. The password is
+hardcoded to 12345678, don't forget to reset it.
+"""
