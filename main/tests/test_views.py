b457338020ead314bfa94bbb06118d8df60467ee
DrEdi <grigoriy.rubel@gmail.com>
2017-01-30 22:40:58 +0200

    Refresh Db in vagga, update test

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -20,8 +20,9 @@ class ViewsTest(TestCase):
         post = Post.objects.create(author=self.admin, title='Test', text='superText', created_date=timezone.now())
         post.publish()
         response = self.client.get('/')
-        self.assertIsInstance(response.context['posts'][0], Post)
         self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'main/index.html')
+        self.assertContains(response, 'Test')
 
     def test_detail_view(self):
         """Testing detail page when post is not exist and when it exists."""
