857cfa6a554f7ec6a515b46ee33a521125c0473e
Grigoriy <grigoriy.rubel@gmail.com>
2017-02-24 00:07:02 +0200

    Update to class based views (#22)

--- main/models.py
+++ main/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
 class Comment(models.Model):
     """Model for comments."""
 
-    post = models.ForeignKey('main.Post', related_name='comments')
+    post = models.ForeignKey('main.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
--- main/views.py
+++ main/views.py
@@ -1,104 +1,136 @@
 """All views are here."""
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post, Comment
-from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404, redirect
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, View, DeleteView, TemplateView
+
+from .forms import CommentForm, PostForm
+from .models import Post, Comment
 
 
-def post_list(request):
+class Protected(View):
+    """Protect views that need to show only for authorised users."""
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        """Rewrite standart method and it to decorator."""
+        return super().dispatch(*args, **kwargs)
+
+
+class PostList(ListView):
     """Show all your Post objects."""
-    posts = Post.objects.filter(published_date__lte=timezone.now())
-    return render(request, 'main/index.html', {'posts': posts})
+
+    context_object_name = 'posts'
+    template_name = 'main/index.html'
+
+    def get_queryset(self):
+        """Return needed posts."""
+        return Post.objects.filter(published_date__lte=timezone.now())
 
 
-def post_detail(request, pk):
+class PostDetail(DetailView):
     """Show info about one post you have chosen."""
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'main/post_detail.html', {'post': post})
+
+    model = Post
+    template_name = 'main/post_detail.html'
 
 
-@login_required
-def post_new(request):
+class NewPost(CreateView, Protected):
     """Return page for adding new Post."""
-    if request.method == 'POST':
+
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'main/post_edit.html'
+
+    def form_valid(self, form):
+        """Add info to form that were not given from POST request."""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        """Get info from form and save it as 'post' object."""
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'main/post_edit.html', {'form': form})
 
 
-@login_required
-def post_edit(request, pk):
+class EditPost(UpdateView, Protected):
     """View is for editing post."""
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'main/post_edit.html', {'form': form})
+
+    model = Post
+    fields = ['title', 'text']
+    success_url = reverse_lazy('post_list')
+    template_name = 'main/post_edit.html'
 
 
-@login_required
-def post_draft_list(request):
+class PostDraftList(ListView, Protected):
     """Return draft list."""
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'main/post_draft_list.html', {'posts': posts})
+
+    queryset = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    context_object_name = 'posts'
+    template_name = 'main/post_draft_list.html'
 
 
-@login_required
-def post_publish(request, pk):
+class PublishPost(Protected, TemplateView):
     """View for publishing post."""
-    post = get_object_or_404(Post, pk=pk)
-    post.publish()
-    return redirect('post_detail', pk=pk)
+
+    def get(self, request, *args, **kwargs):
+        """Get info about POst pk that we use."""
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        post.publish()
+        return redirect('post_detail', pk=post.pk)
 
 
-@login_required
-def post_remove(request, pk):
+class RemovePost(Protected, DeleteView):
     """View for deleting posts."""
-    post = get_object_or_404(Post, pk=pk)
-    post.delete()
-    return redirect('post_list')
+
+    model = Post
+    success_url = reverse_lazy('post_list')
+    template_name = 'main/post_edit.html'
 
 
-def add_comment_to_post(request, pk):
+class AddCommentToPost(CreateView):
     """If someone wants to create new comment he/she get this view."""
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
+
+    model = Comment
+    fields = ['author', 'text']
+    template_name = 'main/post_edit.html'
+
+    def post(self, request, *args, **kwargs):
+        """Add comment to DB."""
+        post = get_object_or_404(Post, pk=kwargs['pk'])
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
             return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'main/add_comment_to_post.html', {'form': form})
 
 
-@login_required
-def comment_approve(request, pk):
+class ApproveComment(Protected, TemplateView):
     """Moderate comment."""
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
+
+    def get(self, request, *args, **kwargs):
+        """Approve comment and update info in DB."""
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        comment.approve()
+        return redirect('post_detail', pk=comment.post.pk)
 
 
-@login_required
-def comment_remove(request, pk):
+class RemoveComment(Protected, DeleteView):
     """Remove comment."""
-    comment = get_object_or_404(Comment, pk=pk)
-    post_pk = comment.post.pk
-    comment.delete()
-    return redirect('post_detail', pk=post_pk)
+
+    model = Comment
+    template_name = 'main/post_edit.html'
+
+    def post(self, request, *args, **kwargs):
+        """Rewritten standart method."""
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        post_pk = comment.post.pk
+        comment.delete()
+        return redirect('post_detail', pk=post_pk)
